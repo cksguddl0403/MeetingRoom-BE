e1f730cac2ce7f1eae6077e991a65fc25fe8ea43
cksguddl0403 <cksguddl0403@naver.com>
2026-04-23 16:11:10 +0900

    4

--- src/main/java/kr/co/ta9/meetingroom/domain/inspection/dto/InspectionListSearchRequestDto.java
+++ src/main/java/kr/co/ta9/meetingroom/domain/inspection/dto/InspectionListSearchRequestDto.java
@@ -1,5 +1,7 @@
 package kr.co.ta9.meetingroom.domain.inspection.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.validation.constraints.AssertTrue;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -22,5 +24,14 @@ public class InspectionListSearchRequestDto {
         this.name = name;
         this.fromDate = fromDate;
         this.toDate = toDate;
+    }
+
+    @JsonIgnore
+    @AssertTrue(message = "시작일은 종료일보다 늦을 수 없습니다.")
+    public boolean isDateRangeValid() {
+        if (fromDate == null || toDate == null) {
+            return true;
+        }
+        return !fromDate.isAfter(toDate);
     }
 }
